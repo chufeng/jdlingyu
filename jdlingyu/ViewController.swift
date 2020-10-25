32cfd4a62d1041c0ffe76106103a6c5c20cbb996
chufeng <chufeng@users.noreply.github.com>
2020-10-26 00:14:53 +0800

    3

--- jdlingyu/ViewController.swift
+++ jdlingyu/ViewController.swift
@@ -12,10 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        nslodq12
+      
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    func 1{
+    
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
